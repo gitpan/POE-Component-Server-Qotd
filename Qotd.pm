c97dbd6ba8420e687942edbd97a084d521c00de4
Chris Williams <chris@bingosnet.co.uk>
2006-08-10 14:34:28 +0000

    Import of BINGOS/POE-Component-Server-Qotd-1.01 from CPAN.
    gitpan-cpan-distribution: POE-Component-Server-Qotd
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         BINGOS/POE-Component-Server-Qotd-1.01.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

--- Qotd.pm
+++ Qotd.pm
@@ -19,21 +19,21 @@ use vars qw($VERSION);
 use constant DATAGRAM_MAXLEN => 1024;
 use constant DEFAULT_PORT => 17;
 
-$VERSION = '1.0';
+$VERSION = '1.01';
 
 sub spawn {
-  my ($package) = shift;
+  my $package = shift;
   croak "$package requires an even number of parameters" if @_ & 1;
 
   my %parms = @_;
 
-  $parms{'Alias'} = 'Qotd-Server' unless ( defined ( $parms{'Alias'} ) and $parms{'Alias'} );
-  $parms{'tcp'} = 1 unless ( defined ( $parms{'tcp'} ) and $parms{'tcp'} == 0 );
-  $parms{'udp'} = 1 unless ( defined ( $parms{'udp'} ) and $parms{'udp'} == 0 );
+  $parms{'Alias'} = 'Qotd-Server' unless defined $parms{'Alias'} and $parms{'Alias'};
+  $parms{'tcp'} = 1 unless defined $parms{'tcp'} and $parms{'tcp'} == 0;
+  $parms{'udp'} = 1 unless defined $parms{'udp'} and $parms{'udp'} == 0;
   $parms{'Quote'} = 'Parts that positively cannot be assembled in improper order will be.'
-	unless ( defined ( $parms{'Quote'} ) and length( $parms{'Quote'} <= 512 ) );
+	unless defined $parms{'Quote'} and length $parms{'Quote'} <= 512;
 
-  my ($self) = bless( { }, $package );
+  my $self = bless { }, $package;
 
   $self->{CONFIG} = \%parms;
 
@@ -54,7 +54,7 @@ sub accept_new_client {
   my ($kernel,$self,$socket,$peeraddr,$peerport,$wheel_id) = @_[KERNEL,OBJECT,ARG0 .. ARG3];
   $peeraddr = inet_ntoa($peeraddr);
 
-  my ($wheel) = POE::Wheel::ReadWrite->new (
+  my $wheel = POE::Wheel::ReadWrite->new (
         Handle => $socket,
         Filter => POE::Filter::Line->new(),
         InputEvent => 'client_input',
@@ -62,21 +62,21 @@ sub accept_new_client {
 	FlushedEvent => 'client_flushed',
   );
 
-  $self->{Clients}->{ $wheel->ID() }->{Wheel} = $wheel;
-  $self->{Clients}->{ $wheel->ID() }->{peeraddr} = $peeraddr;
-  $self->{Clients}->{ $wheel->ID() }->{peerport} = $peerport;
+  $self->{Clients}->{ $wheel->ID() } = { Wheel => $wheel, peeraddr => $peeraddr, peerport => $peerport };
   $wheel->put( $self->{CONFIG}->{Quote} );
+  undef;
 }
 
 sub client_input {
   my ($kernel,$self,$input,$wheel_id) = @_[KERNEL,OBJECT,ARG0,ARG1];
+  undef;
 }
 
 sub client_flushed {
   my ($kernel,$self,$wheel_id) = @_[KERNEL,OBJECT,ARG0];
-
-  delete ( $self->{Clients}->{ $wheel_id }->{Wheel} );
-  delete ( $self->{Clients}->{ $wheel_id } );
+  delete $self->{Clients}->{ $wheel_id }->{Wheel};
+  delete $self->{Clients}->{ $wheel_id };
+  undef;
 }
 
 sub get_datagram {
@@ -87,7 +87,7 @@ sub get_datagram {
 
   send( $socket, $self->{CONFIG}->{Quote}, 0, $remote_address ) == length( $self->{CONFIG}->{Quote} )
       or warn "Trouble sending response: $!";
-
+  undef;
 }
 
 1;
@@ -99,9 +99,9 @@ POE::Component::Server::Qotd - a POE component implementing a RFC 865 QotD serve
 
 =head1 SYNOPSIS
 
-use POE::Component::Server::Qotd;
+ use POE::Component::Server::Qotd;
 
- my ($self) = POE::Component::Server::Qotd->spawn( 
+ my $self = POE::Component::Server::Qotd->spawn( 
 	Alias => 'Chargen-Server',
 	BindAddress => '127.0.0.1',
 	BindPort => 7777,
@@ -119,7 +119,17 @@ L<POE|POE>. It is a class inherited from L<POE::Component::Server::Echo|POE::Com
 
 =item spawn
 
-Takes a number of optional values: "Alias", the kernel alias that this component is to be blessed with; "BindAddress", the address on the local host to bind to, defaults to L<POE::Wheel::SocketFactory|POE::Wheel::SocketFactory> default; "BindPort", the local port that we wish to listen on for requests, defaults to 19 as per RFC, this will require "root" privs on UN*X; "options", should be a hashref, containing the options for the component's session, see L<POE::Session|POE::Session> for more details on what this should contain; "Quote", text to be sent to connecting clients, default quote is applied if this is not supplied or is greater than 512 characters.
+Takes a number of optional values: 
+
+  "Alias", the kernel alias that this component is to be blessed with; 
+  "BindAddress", the address on the local host to bind to, defaults to 
+                 POE::Wheel::SocketFactory default; 
+  "BindPort", the local port that we wish to listen on for requests, 
+	      defaults to 19 as per RFC, this will require "root" privs on UN*X; 
+  "options", should be a hashref, containing the options for the component's session, 
+             see POE::Session for more details on what this should contain; 
+  "Quote", text to be sent to connecting clients, default quote is applied if this 
+           is not supplied or is greater than 512 characters.
 
 =back
 
@@ -131,14 +141,16 @@ Report any bugs through L<http://rt.cpan.org/>.
 
 Chris 'BinGOs' Williams, <chris@bingosnet.co.uk>
 
-The character generation code by Rocco Caputo.
-
 =head1 SEE ALSO
 
-L<POE|POE>
-L<POE::Session|POE::Session>
-L<POE::Wheel::SocketFactory|POE::Wheel::SocketFactory>
-L<POE::Component::Server::Echo|POE::Component::Server::Echo>
+L<POE>
+
+L<POE::Session>
+
+L<POE::Wheel::SocketFactory>
+
+L<POE::Component::Server::Echo>
+
 L<http://www.faqs.org/rfcs/rfc865.html>
 
 =cut
